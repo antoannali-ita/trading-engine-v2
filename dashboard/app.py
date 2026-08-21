b85f3aaba8643b4f4a501267f83c4e7993ceb654
Antonio <antoannali@gmail.com>
2026-08-21 22:20:36 +0200

    Use stateful contextual dashboard UI v8

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
 try:
-    from dashboard.ui_v7 import *  # noqa: F401,F403
+    from dashboard.ui_v8 import *  # noqa: F401,F403
 except ModuleNotFoundError:
-    from ui_v7 import *  # noqa: F401,F403
+    from ui_v8 import *  # noqa: F401,F403
