db147a00109a39b78a6e3d3632b2a5f28f56e06c
Antonio <antoannali@gmail.com>
2026-08-21 18:35:11 +0200

    feat: enable dashboard ui v6

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
 try:
-    from dashboard.ui_v5 import *  # noqa: F401,F403
+    from dashboard.ui_v6 import *  # noqa: F401,F403
 except ModuleNotFoundError:
-    from ui_v5 import *  # noqa: F401,F403
+    from ui_v6 import *  # noqa: F401,F403
